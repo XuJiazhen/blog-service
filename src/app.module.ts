40c2dd7ba006f5e52910b27904e2447eae6b58fe
XuJiazhen <xu.159623@gmail.com>
2020-03-16 05:02:40 +0800

    initialize article module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ArticleModule } from './module/article/article.module';
 
 @Module({
-  imports: [],
+  imports: [ArticleModule],
   providers: [],
 })
 export class AppModule {}
