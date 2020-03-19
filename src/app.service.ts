91acedbca7de911d6a0f22473e0cc43843b76c89
XuJiazhen <xu.159623@gmail.com>
2020-03-19 12:27:22 +0800

    modified app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from './module/config/config.service';
-
 @Injectable()
-export class AppService {
-  constructor(config: ConfigService) {
-    console.log(config.MONGO_URL);
-  }
-}
+export class AppService {}
