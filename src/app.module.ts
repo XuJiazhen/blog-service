e4f71d6e04d760f2c019259d70f9143a86607fc5
XuJiazhen <xu.159623@gmail.com>
2020-03-19 12:34:09 +0800

    init auth dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ArticleModule } from './module/article/article.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from './module/config/config.module';
 import { AppService } from './app.service';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/service'),
     ArticleModule,
     ConfigModule,
+    AuthModule,
   ],
   providers: [AppService],
 })
